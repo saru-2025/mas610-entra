e90611eee1c054d3134c648fb4a458c5c21291a6
Saritha Mpragada <saritha.mpragada@outlook.com>
2025-11-03 16:37:56 +0800

    Fix provider env access: use env.VAR instead of env()

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,5 @@
 provider "databricks" {
-  host  = var.databricks_host != "" ? var.databricks_host : env("DATABRICKS_HOST")
-  token = var.databricks_token != "" ? var.databricks_token : env("DATABRICKS_TOKEN")
+  # prefer explicitly passed variable, otherwise read from environment
+  host  = var.databricks_host != "" ? var.databricks_host : env.DATABRICKS_HOST
+  token = var.databricks_token != "" ? var.databricks_token : env.DATABRICKS_TOKEN
 }
